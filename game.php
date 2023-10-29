15d32da0f49aa12f3484730d542e051155a095c3
Chris-D7 <constantincristian.draghici@ulbsibiu.ro>
2023-10-30 00:28:17 +0100

    Menu + Game CSS

--- game.php
+++ game.php
@@ -5,6 +5,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="game.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo&display=swap" rel="stylesheet">
 </head>
 <script>
     function setStaffId(staffId, salary) {
@@ -51,7 +54,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="hireStaffModalLabel">Hire Staff</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <form action="hire_staff.php" method="POST">
@@ -92,7 +94,6 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="displayHiredStaffModalLabel">Hired Staff</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <ul>
--- menu.php
+++ menu.php
@@ -4,6 +4,9 @@
     <title>Menu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="menu.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo&display=swap" rel="stylesheet">
 </head>
 <body class="bg-light">
     <div class="container">
@@ -27,7 +30,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="rulesModalLabel">Game Rules</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <p>Rule 1: ...</p>
