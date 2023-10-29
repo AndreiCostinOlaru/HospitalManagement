8dec5b81ab20974d6b08d0953757e78be3a22efc
AndreiCostinOlaru <125856221+AndreiCostinOlaru@users.noreply.github.com>
2023-10-29 15:03:06 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,7 +3,8 @@
 <head>
     <title>Menu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script></head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</head>
 <body class="bg-light">
     <div class="container">
         <div class="row justify-content-center mt-5">
@@ -12,8 +13,28 @@
                     <div class="card-body">
                         <h2 class="card-title">Welcome to the Game Menu</h2>
                         <p>Menu content goes here.</p>
-                        <a href="logout.php" class="btn btn-danger">Logout</a>
+                        <a href="game.php" class="btn btn-primary">START</a>
+                        <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#rulesModal">RULES</button>
+                        <a href="logout.php" class="btn btn-danger">LOG OUT</a>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="rulesModal" tabindex="-1" aria-labelledby="rulesModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="rulesModalLabel">Game Rules</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Rule 1: ...</p>
+                    <p>Rule 2: ...</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
